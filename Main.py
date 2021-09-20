24a93d5a2f19b4aee475dfe84a582f4e20e8dc56
Morse <enmorse48@gmail.com>
2021-09-19 21:42:23 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Use a variable to represent a person's name, and print
+# a message to that person.
+
+first_name = "ernest"
+last_name = "morse"
+full_name = f"{first_name.title()} {last_name.title()}"
+message = f"Hello {full_name}, would you like to learn " \
+          f"some Python today?"
+print(message)
